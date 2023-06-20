1e6da0ca2e187b3e41901dc53b8f12d2324d4992
miz092 <mihalyfi.zoltan@gmail.com>
2023-06-20 15:05:17 +0200

    add request functions to service

--- todoApp/src/app/service/input.service.ts
+++ todoApp/src/app/service/input.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../model/task';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,31 @@ export class InputService {
 
   constructor(private http: HttpClient) {
     this.URL = 'http://something:6969/xy';
+  }
+
+  async getAllTasks(): Promise<Task[]> {
+    const data = await fetch(this.URL);
+    return (await data.json()) ?? [];
+  }
+
+  async getTaskById(id: number): Promise<Task | undefined> {
+    const data = await fetch(`${this.URL}/${id}`);
+    return (await data.json()) ?? {};
+  }
+
+  async createTask(task: Task): Promise<Task> {
+    const response = await fetch(this.URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+
+    return response.json();
   }
 }
