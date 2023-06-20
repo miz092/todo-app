e786e26f491bc38c772725090b10d619da19abf3
miz092 <mihalyfi.zoltan@gmail.com>
2023-06-20 14:43:59 +0200

    change task class into interface

--- todoApp/src/app/model/task.ts
+++ todoApp/src/app/model/task.ts
@@ -1,7 +1,7 @@
-export class Task {
-  id: number = 0;
-  title: string = '';
-  description: string = '';
-  number: Date = new Date(Date.now());
-  isComplete: boolean = false;
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  date: Date;
+  isComplete: boolean;
 }
